05315128d72d5f54a29f05bc25393dfea0763da7
Raf Meeusen <raf.meeusen@telit.com>
2018-07-18 13:10:37 +0200

    euler prob14: added code to find longest seq until 100.000

--- projecteuler/problem14/problem14.py
+++ projecteuler/problem14/problem14.py
@@ -15,4 +15,17 @@ def collatz_seq(num):
 print('example: Collatz sequence for number 51:')
 print(collatz_seq(51))
 
- 
+maxnum=100000
+longest_len=0
+longest_num=0
+for i in range(1,maxnum):
+    seqlen=len(collatz_seq(i))
+    if seqlen > longest_len:
+        #print(seqlen)
+        longest_len=seqlen
+        longest_num=i
+
+print('checked Collatz sequence lengths between 1 and ',maxnum)
+print('longest sequence found for ',longest_num)
+print('sequence length: ',longest_len)
+print(collatz_seq(longest_num))
