dd5e8637dbdc638e11e8850fe66d63692b2203ff
Raf Meeusen <raf.meeusen@telit.com>
2019-05-07 09:39:03 +0200

    made start to print page info

--- pdf_parser/pdf_parse.py
+++ pdf_parser/pdf_parse.py
@@ -2,6 +2,7 @@ import argparse
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
+from itertools import islice
 
 class PdfParser(object):
     ''' basic CLI tool to extra info from a pdf,
@@ -18,14 +19,17 @@ class PdfParser(object):
         return nrpages
     def getdocinfo(self):
         return self.doc.info[0]
-
-
+    def getpage(self, pagenr):
+        pages = PDFPage.create_pages(self.doc)
+        ''' pagenr starts at 1, index in islice at 0 '''
+        return next(islice(pages,pagenr-1,pagenr))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse pdf files')
     parser.add_argument('filename', help='Filename of pdf file')
     parser.add_argument('-i', '--info', action="store_true", help='Print info about the pdf')
+    parser.add_argument('-p', '--page', type=int, help='Print detailed info on specific page in the pdf')
     args = parser.parse_args()
 
     openedpdffile = open(args.filename,'rb')
@@ -38,7 +42,9 @@ if __name__ == '__main__':
         for key in info.keys():
             print(key, ":", info[key])
         print('Nr of pages:', nrpages)
+    if args.page:
+        ''' note: value 0 is ignored this way; pages start at 1 '''
+        print('Detailed info for page ',args.page)
+        print(mypdfparser.getpage(args.page))
 
-'''
-[{'Author': b'Registered to: SD WORX ', 'CreationDate': b'8/28/2018 21:54:47', 'Creator': b'HP Exstream Version 9.0.107 64-bit', 'Title': b'Loonbrief'}]
-'''
+
