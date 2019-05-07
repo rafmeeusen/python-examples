0e55b881d707d0bfdf5f4dd00480613ab6970db0
Raf Meeusen <raf.meeusen@telit.com>
2019-05-07 10:09:32 +0200

    pdf parser printing overview of page child objects

--- pdf_parser/pdf_parse.py
+++ pdf_parser/pdf_parse.py
@@ -3,6 +3,14 @@ from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
 from itertools import islice
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.layout import LAParams
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.layout import LTLine
+from pdfminer.layout import LTRect
+from pdfminer.layout import LTFigure
+
 
 class PdfParser(object):
     ''' basic CLI tool to extra info from a pdf,
@@ -11,6 +19,10 @@ class PdfParser(object):
     def __init__(self, fp):
         parser = PDFParser(fp)
         self.doc = PDFDocument(parser)
+        laparams = LAParams()
+        rsrcmgr = PDFResourceManager()
+        self.device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        self.interpreter = PDFPageInterpreter(rsrcmgr, self.device)
     def nrofpages(self):
         pages = PDFPage.create_pages(self.doc)
         nrpages = 0
@@ -19,10 +31,13 @@ class PdfParser(object):
         return nrpages
     def getdocinfo(self):
         return self.doc.info[0]
-    def getpage(self, pagenr):
+    def getpagelayout(self, pagenr):
         pages = PDFPage.create_pages(self.doc)
         ''' pagenr starts at 1, index in islice at 0 '''
-        return next(islice(pages,pagenr-1,pagenr))
+        page_x = next(islice(pages,pagenr-1,pagenr))
+        self.interpreter.process_page(page_x)
+        layout = self.device.get_result()
+        return layout
 
 
 if __name__ == '__main__':
@@ -42,9 +57,28 @@ if __name__ == '__main__':
         for key in info.keys():
             print(key, ":", info[key])
         print('Nr of pages:', nrpages)
+
+    pl=None
     if args.page:
         ''' note: value 0 is ignored this way; pages start at 1 '''
-        print('Detailed info for page ',args.page)
-        print(mypdfparser.getpage(args.page))
+        print('Detailed info for page',args.page)
+        pl=mypdfparser.getpagelayout(args.page)
+        cntr_lines=0
+        cntr_rect=0
+        cntr_fig=0
+        for child_object in pl:
+            ''' ignore lines '''
+            if isinstance(child_object,LTLine):
+                cntr_lines += 1
+            elif isinstance(child_object,LTRect):
+                cntr_rect += 1
+            elif isinstance(child_object,LTFigure):
+                cntr_fig += 1
+            else:
+                print(child_object)
+        print("note: skipped following:")
+        print("\tLTLine objects:", cntr_lines)
+        print("\tLTRect objects:", cntr_rect)
+        print("\tLTFigure objects:", cntr_fig)
 
 
